08951ce38792b8f602cf326c40ed186670ff3875
jenniferkwok <jenwkwok@gmail.com>
2015-10-13 22:10:40 -0700

    testing

--- base.js
+++ base.js
@@ -2,6 +2,19 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+//delete button
+$( document ).on( "click", "#delete", function() {
+    $(this.parentElement.parentElement).remove();
+} ); 
+
+$("#submit").on('click', function(){
+	event.preventDefault();
+	var newBlogPost = $('#content').val();
+	console.log(newBlogPost);
+	var deleteButton = '<button id="delete" class="btn btn-danger"><span class="glyphicon glyphicon-minus" aria-hidden="true"></span></button>';
+$('#blogContent').append('<div class="panel panel-default"><div class="panel-body">'+ newBlogPost+deleteButton+'</div></div>');
+
+});
+
 
 });
